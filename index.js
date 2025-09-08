f1f49e730664836bedab667fd745ca728fd3b480
rkterungwa16 <terungwakombol@gmail.com>
2025-09-08 06:58:20 +0100

    use env file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { Routers } = require("./router");
 const { HomeTemplate } = require("./route-handlers/home.template.js");
 const { HomeAssets } = require("./route-handlers/home.assets.js");
 
+const port = process.env.PORT || 3300;
+
 Routers.register("client", HomeAssets).register('/', HomeTemplate);
 
 /**
@@ -10,15 +12,15 @@ Routers.register("client", HomeAssets).register('/', HomeTemplate);
  * @param {http.IncomingMessage} req - The incoming request object.
  * @param {http.ServerResponse} res - The server response object.
  */
-const process = function (req, res) {
+const routes = function (req, res) {
   Routers.check(req.url, [req, res]);
 };
 
 // Instantiate the HTTP server
-const server = http.createServer(process).listen(3300, function () {
+const server = http.createServer(routes).listen(port, function () {
   console.log(
     "\x1b[36m%s\x1b[0m",
-    "The HTTP server is running on port " + 3300
+    "The HTTP server is running on port " + port
   );
 });
 
